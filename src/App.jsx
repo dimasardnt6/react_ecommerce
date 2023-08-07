9524c620e0872eb6c0eec0a4fd5c6623aedc3af4
Dimas Ardianto <dimasardnt6@gmail.com>
2023-08-07 13:13:23 +0700

    Update progres

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Details from './pages/Details'
 import Cart from './pages/Cart'
+import Congratulation from './pages/Congratulation'
+import NotFound from './pages/NotFound'
 
 
 export default function App() {
@@ -14,6 +16,8 @@ export default function App() {
           <Route exact path="/" element={<HomePage/>} />
           <Route path="/categories/:idc" element={<Details/>} />
           <Route path="/cart" element={<Cart/>} />
+          <Route path="/congratulation" element={<Congratulation/>} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
       </Router>
     </div>
